eebfc29899e18f82ec14b7f12775bb76ce04a835
ynakahashi <ynakahashi1003@gmail.com>
2019-03-12 19:49:17 +0900

    TorchTextの中身を見ている

--- Torchtext.py
+++ Torchtext.py
@@ -6,11 +6,10 @@ Created on Thu Mar  7 16:56:26 2019
 @author: yn250006
 """
 
-""" Set current directory """
-cd /Users/yn250006/Documents/GitHub/Try_Pytorch/
+""" ライブラリの読み込み """
+import os
+import inspect
 
-
-""" Load libraries """
 import janome
 from janome.tokenizer import Tokenizer
 
@@ -28,74 +27,142 @@ from torch.autograd import Variable
 torch.manual_seed(0)
 torch.cuda.manual_seed(0)
 
-""" Define Tokenizer """
+""" Working Directory をセット """
+os.chdir("/Users/yn250006/Documents/GitHub/Try_Pytorch/")
+
+
+""" Tokenizer をセット """
 j_t = Tokenizer()
 def tokenizer(text): 
     return [tok for tok in j_t.tokenize(text, wakati=True)]
 
-TEXT = data.Field(sequential=True, tokenize=tokenizer, lower=True, include_lengths=True, batch_first=True)
-LABEL = data.Field(sequential=False, use_vocab=False)
+""" 試してみる """
+text = "私は明日、晴れたら遠足に行きます"
+print(j_t.tokenize(text, wakati=True))
 
-""" Read data under the above definition """
+""" Field のインスタンス化 """
+TEXT = data.Field(sequential = True, 
+                  tokenize = tokenizer, 
+                  lower = True, 
+                  include_lengths = True,
+                  batch_first = True)
+
+LABEL = data.Field(sequential = False, 
+                   use_vocab = False)
+
+print(inspect.getsource(data.Field))
+
+
+""" インスタンス化したFieldを使ってデータを読み込む """
 # train, val, test = data.TabularDataset.splits(
 #         path='./Data/', train='train.tsv',
 #         validation='valid.tsv', test='test.tsv', format='tsv',
 #         fields=[('Text', TEXT), ('Label', LABEL)])
 train, val, test = data.TabularDataset.splits(
-        path='./Data/', train='train_small.tsv',
-        validation='valid_small.tsv', test='test_small.tsv', format='tsv',
-        fields = [('Text', TEXT), ('Label', LABEL)])
+    path = './Data/', 
+    train = 'train_small.tsv',
+    validation = 'valid_small.tsv',
+    test = 'test_small.tsv',
+    format = 'tsv',
+    fields = [('Text', TEXT), ('Label', LABEL)])
 
-""" Check """"
+""" 
+data.TabularDataset.splits() は cls(path/to/data, **kwargs) が本体の様子
+cls は Python の classmethod の記法で、class自身が入る
+なので data.TabularDataset.splits(cls) は結局 data.TabularDataset()になる
+ちなみに Python の第一引数で使われる self はインスタンス自身が入る
+"""
+print(inspect.getsource(data.TabularDataset.splits))
+tmp = data.TabularDataset(os.path.join("./Data", "train_small.tsv"), 
+    # path = './Data/', 
+    # train = 'train_small.tsv',
+    # validation = 'valid_small.tsv',
+    # test = 'test_small.tsv',
+    format = 'tsv',
+    fields = [('Text', TEXT), ('Label', LABEL)])
+
+tmp.examples[1].Text[0:10]
+
+
+""" 内容を確認 """"
 print('len(train)', len(train))
 print('vars(train[0])', vars(train[0]))
+train.fields.items()
+train.examples[2].Text[0:10]
 
 print('len(val)', len(val))
 print('vars(val[0])', vars(val[0]))
+val.fields.items()
+val.examples[10].Text[0:10]
 
 print('len(test)', len(test))
 print('vars(test[0])', vars(test[0]))
 
 
-""" 読み込んだデータに出現した単語のリストを作成し、単語に番号を振る """
-TEXT.build_vocab(train, min_freq=10)
+""" 
+読み込んだデータに出現した単語のリストを作成し、単語に番号を振る 
+処理の本体は TEXT.vocab_cls の様子
+"""
+TEXT.build_vocab(train, min_freq = 10)
+print(inspect.getsource(TEXT.build_vocab))
+print(inspect.getsource(TEXT.vocab_cls))
+
 """ FastTextの学習済みベクトルを使うならこちら """
 # TEXT.build_vocab(train, vectors = FastText(language="ja"), min_freq = 10)
 
 
-""" Check """
-""" dictionaryはsliceできないけど、一度listに変換すればsliceできる """
+""" 
+内容を確認する
+dictionary は slice できないけど、一度 list に変換すれば slice できる
+"""
 # TEXT.vocab.freqs
-dict(list(TEXT.vocab.freqs.items())[0:10])
+dict(list(TEXT.vocab.freqs.items())[0:20])
+
 # TEXT.vocab.stoi
-dict(list(TEXT.vocab.stoi.items())[0:10])
+dict(list(TEXT.vocab.stoi.items())[0:20])
+
 # TEXT.vocab.itos
-dict(list(TEXT.vocab.stoi.items())[0:10])
+TEXT.vocab.itos[0:20]
 
 
 """
 data.Iterator.splits は dataset オブジェクトから、各単語を番号に変換して
 ミニバッチごとにまとめた行列を返すイテレータを作成できます。
+
+data.Iterator.splits() は data.TabularDataset.splits() と同様に classmethod
+なので data.Iterator() と同じ
 """
-
+#train_iter, val_iter, test_iter = data.Iterator.splits(
+#        (train, val, test), batch_sizes=(5, 256, 256), device=-1, repeat=False,sort=False)
 train_iter, val_iter, test_iter = data.Iterator.splits(
-        (train, val, test), batch_sizes=(32, 256, 256), device=-1, repeat=False,sort=False)
+        (train, val, test), 
+        batch_sizes=(16, 1, 1), 
+        device="cpu", 
+        repeat=False, 
+        sort=False)
 
-
-train_iter, val_iter, test_iter = data.Iterator.splits(
-        (train, val, test), batch_sizes=(32, 2, 1), device=-1, repeat=False,sort=False)
-
+print(inspect.getsource(data.Iterator.splits))
+print(len(train))
+tmp = data.Iterator(train, batch_size=(32, 256, 256), repeat=False, sort=False)
+tmp.data()[0:10]
+iter(tmp)
+next(iter(tmp.dataset))
 
 """
 イテレータの返す結果
-batch.Textは[32, 1475]の二次元配列と[32]の一次元配列を含むtuple、
-batch.Labelは[32]の一次元配列
+batch.Textは[n, m]の二次元配列と[n]の一次元配列を含むtuple
+batch.Labelは[n]の一次元配列
+
+n は batch_size の 1 番目の数値
 """
 batch = next(iter(train_iter))
+
 print(batch.Text)
-print(batch.Label)
 batch.Text[0].size()
+
+print(batch.Label)
 batch.Text[1].size()
+
 
 # class EncoderRNN(nn.Module):
 #     def __init__(self, emb_dim, h_dim, v_size, gpu=True, batch_first=True):
@@ -156,14 +223,15 @@ class Attn(nn.Module):
 
     def forward(self, encoder_outputs):
         b_size = encoder_outputs.size(0)
-        attn_ene = self.main(encoder_outputs.view(-1, self.h_dim)) # (b, s, h) -> (b * s, 1)
+        # attn_ene = self.main(encoder_outputs.view(-1, self.h_dim)) # (b, s, h) -> (b * s, 1)
+        attn_ene = self.main(encoder_outputs.contiguous().view(-1, self.h_dim)) # (b, s, h) -> (b * s, 1)
         return F.softmax(attn_ene.view(b_size, -1), dim=1).unsqueeze(2) # (b*s, 1) -> (b, s, 1)
 
 
 class AttnClassifier(nn.Module):
     def __init__(self, h_dim, c_num):
         super(AttnClassifier, self).__init__()
-        self.attn = f
+        self.attn = Attn(h_dim)
         self.main = nn.Linear(h_dim, c_num)
 
 
@@ -190,7 +258,8 @@ def train_model(epoch, train_iter, optimizer, log_interval=1, batch_size=2):
             print('train epoch: {} [{}/{}], acc:{}, loss:{}'.format(
             epoch, (idx+1)*len(x), len(train_iter)*batch_size,
             correct/float(log_interval * len(x)),
-            loss.data[0]))
+            loss.data.item()))
+            #loss.data[0]))
             correct = 0
 
             
@@ -215,14 +284,14 @@ lr = 0.001 #学習係数
 epochs = 50 #エポック数
 
  # make model
-encoder = EncoderRNN(emb_dim, h_dim, len(TEXT.vocab),gpu=False, v_vec = TEXT.vocab.vectors)
+encoder = EncoderRNN(emb_dim, h_dim, len(TEXT.vocab), gpu=False, v_vec = TEXT.vocab.vectors)
 classifier = AttnClassifier(h_dim, class_num)
 
 # init model
 def weights_init(m):
     classname = m.__class__.__name__
     if hasattr(m, 'weight') and (classname.find('Embedding') == -1):
-        nn.init.xavier_uniform(m.weight.data, gain=nn.init.calculate_gain('relu'))
+        nn.init.xavier_uniform_(m.weight.data, gain=nn.init.calculate_gain('relu'))
 
 for m in encoder.modules():
     print(m.__class__.__name__)
@@ -242,6 +311,27 @@ for epoch in range(epochs):
     train_model(epoch + 1, train_iter, optimizer)
     test_model(epoch + 1, val_iter)
 
+for epoch in range(epochs):
+    train_model(epoch + 1, train_iter, optimizer)
 
 
 
+
+
+for idx, batch in enumerate(train_iter):
+    (x, x_l), y = batch.Text, batch.Label
+    optimizer.zero_grad()
+    encoder_outputs = encoder(x)
+    output, attn = classifier(encoder_outputs)
+    loss = F.nll_loss(output, y)
+    loss.backward()
+    optimizer.step()
+    pred = output.data.max(1, keepdim=True)[1]
+    correct += pred.eq(y.data.view_as(pred)).cpu().sum()
+    if idx % log_interval == 0:
+        print('train epoch: {} [{}/{}], acc:{}, loss:{}'.format(
+                epoch, (idx+1)*len(x), len(train_iter) * batch_size,
+                correct/float(log_interval * len(x)),
+                loss.data.item()))
+        #loss.data[0]))
+        correct = 0
